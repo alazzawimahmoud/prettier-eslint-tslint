2eb746802d74936dc11b8ba569b4f9b8bc5d3d17
Azzawi <mahmoud.alazzawi@seaters.com>
2018-07-19 09:09:09 +0200

    testing precommit

---
+++ app/index.ts
@@ -0,0 +1,15 @@
+// tslint:disable-next-line:no-empty-interface
+interface Empty { }
+
+const world = () => console.log();
+
+function testing(params: number) {
+    if (true) {
+        console.log("baba");
+    }
+    const superWorld2 = "kdjkf";
+    const superWorld3 = "kdjkf";
+    const superWorld = "kdjkf";
+    console.log({ superWorld });
+    return "name";
+}
---
+++ test.ts
@@ -0,0 +1,12 @@
+interface EmptyPart { name: string; }
+
+const theWorld = () => console.log();
+
+function theTesting(params: number) {
+    if (true) {
+        console.log("baba");
+    }
+    const superWorld = "kdjkf";
+    console.log({ superWorld });
+    return "name";
+}
